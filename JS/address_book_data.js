18dd9e900275d7f173b8712610d0259941b3ec91
Yogesh More <iyogesh.more1998@gmail.com>
2022-06-11 13:56:37 +0530

    Added : Form Validations.

---
+++ JS/address_book_data.js
@@ -0,0 +1,76 @@
+// <!-- UC-4 - Add Form Validations -->
+
+class Contact {
+
+    get id() {
+    return this._id;
+    }
+    set id(id) {
+    this._id = id;
+    }
+
+    get name() {
+    return this._name;
+    }
+    set name(name) {
+    let nameRegex = RegExp("^[A-Z]{1}[A-Za-z\\s]{2,}$"); //must start with Capital Letter and must have at least 3 Characters
+    if (nameRegex.test(name)) {
+    this._name = name;
+    } else {
+    throw "Name is Invalid!";
+    }
+    }
+
+    get address() {
+    return this._address;
+    }
+    set address(address) {
+    let addressRegex = RegExp("^[a-zA-Z]{1}[a-zA-Z\\s]{2,}$"); //should have multiple words and each word should be at least 3 characters
+    if (addressRegex.test(address)) {
+    this._address = address;
+    } else {
+        throw "Address is Invalid!";
+    }
+    }
+
+    get city() {
+    return this._city;
+    }
+    set city(city) {
+    this._city = city;
+    }
+
+    get state() {
+    return this._state;
+    }
+    set state(state) {
+    this._state = state;
+    }
+
+    get zip() {
+    return this._zip;
+    }
+    set zip(zip) {
+    let zipRegex = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
+    if (zipRegex.test(zip)) {
+        this._zip = zip;
+    } else {
+        throw "Zip is Invalid!";
+    }
+    }
+
+    get phoneNumber() {
+    return this._phoneNumber;
+    }
+    set phoneNumber(phoneNumber) {
+    let phoneNumberRegex = RegExp("^[0-9]{2}\\s{1}[7-9]{1}[0-9]{9}$");
+    if (phoneNumberRegex.test(phoneNumber)) {
+        this._phoneNumber = phoneNumber;
+    } else {
+        throw "Phone Number is Invalid!";
+    }   
+    }
+
+toString(){
+return "Id = " + this.id + ", Name = " + this.name + ", Address = " + this.address + ", City = " + this.city + ", state = " + this.state + ", zip = " + this.zip + ", phone Number = " + this.phoneNumber};
+}
